420272d866d6c29babec4d10e2438e1149d60441
Ergun Batuhan Öktem <batuhanoktem@gmail.com>
2019-05-01 01:33:56 +0300

    Elmah, Entity Framework and Swagger have been added. .NET Framework libraries have been changed to .NET Standard 2.0.

---
+++ Algorithm.Api/DataContexts/AlgorithmContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Algorithm.Api.DataContexts
+{
+    public class AlgorithmContext : DbContext
+    {
+        public AlgorithmContext(DbContextOptions<AlgorithmContext> options)
+            : base(options)
+        {
+        }
+    }
+}
--- Algorithm.Api/Startup.cs
+++ Algorithm.Api/Startup.cs
@@ -2,14 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Algorithm.Api.DataContexts;
+using ElmahCore.Mvc;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Algorithm.Api
 {
@@ -27,11 +31,37 @@ namespace Algorithm.Api
         {
             services.AddControllers()
                 .AddNewtonsoftJson();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Algorithm API", Version = "v1" });
+            });
+
+            services.AddElmah();
+
+            switch (Configuration.GetSection("ActiveProvider").Value)
+            {
+                case "MSSQL":
+                    services.AddDbContext<AlgorithmContext>(options => options.UseSqlServer(Configuration.GetConnectionString("AlgorithmMSSQLContext")));
+                    break;
+                case "PostgreSQL":
+                    services.AddEntityFrameworkNpgsql().AddDbContext<AlgorithmContext>(options => options.UseNpgsql(Configuration.GetConnectionString("AlgorithmPostgreSQLContext")));
+                    break;
+                default:
+                    break;
+            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Algorithm API V1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -41,6 +71,8 @@ namespace Algorithm.Api
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseElmah();
 
             app.UseHttpsRedirection();
 
